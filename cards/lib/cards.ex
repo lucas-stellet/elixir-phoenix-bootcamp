4e5b10206d27af7d9230b1118e53b19a71a966e2
Lucas Stellet <lustepe@gmail.com>
2021-05-17 00:59:11 -0300

    finished section 1

---
+++ cards/lib/cards.ex
@@ -0,0 +1,23 @@
+defmodule Cards do
+  @moduledoc false
+  def create_deck do
+    values = ["ace", "two", "three", "four", "five"]
+    suits = ["spades", "clubs", "hearts", "diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+end
