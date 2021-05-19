2202ea936a8b6763a7999ad23953e714e50a87bc
Lucas Stellet <lustepe@gmail.com>
2021-05-19 00:54:57 -0300

    add dialyxir module

--- cards/mix.exs
+++ cards/mix.exs
@@ -22,7 +22,8 @@ defmodule Cards.MixProject do
   defp deps do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.4", only: [:dev]}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
