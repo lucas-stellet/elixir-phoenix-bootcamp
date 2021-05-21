2d0c3ab720107b89eb59039b47dd0e695389b06a
Lucas Stellet <lustepe@gmail.com>
2021-05-21 17:57:06 -0300

    lesson 49

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,22 +1,47 @@
 defmodule Identicon do
   @moduledoc false
 
+  require Integer
+
   def main(input) do
     input
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
   end
 
-  def mirror_row(grid) do
-    # Enum.drop(grid, -1)
-    Enum.map(grid, fn [f, s, t] = _r -> [f, s, t, s, f] end)
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    Enum.map(grid, fn {_, index} ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end)
   end
 
-  def build_grid(image) do
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    odd_grid = Enum.filter(grid, fn {code, _} -> Integer.is_even(code) end)
+
+    %Identicon.Image{image | grid: odd_grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
     grid =
-      Enum.chunk_every(image.hex, 3, :discard)
-      |> mirror_row()
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
 
     %Identicon.Image{image | grid: grid}
   end
